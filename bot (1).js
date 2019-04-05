c2b2f310212b0b0486fc4a32c3cb9583f2bd3206
meexbak <49203142+meexbak@users.noreply.github.com>
2019-04-05 17:53:45 +0300

    Add files via upload

---
+++ bot (1).js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+
+client.on('ready', () => {
+   console.log(`This Bots Online ' `);
+});
+
+
+
+
+
+
+
+
+
+لاكود
+
+
+
+
+
+
+
+client.login(process.env.TOKEN);
